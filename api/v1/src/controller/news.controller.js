2eb6f5301f827b22d028df6afe3643ae9f6a22a4
Rodrigo Faria <rodrigolfsi@gmail.com>
2020-10-11 08:40:07 -0300

    Change database schema to use UUID as Primary Key

--- api/v1/src/controller/news.controller.js
+++ api/v1/src/controller/news.controller.js
@@ -2,13 +2,7 @@ const { v4: uuid4 } = require('uuid')
 const database = require('../db/postgre.service')
 
 const createUserUUID = async ctx => {
-  const uuid = await getUUID()
-
-  if (!uuid) {
-    ctx.body = 'Failured to generate the UUID. Please, try again.'
-    ctx.status = 508
-    return
-  }
+  const uuid = uuid4()
 
   await database.query('insert into project(uuid, name) values ($1, $2)', [uuid, ctx.request.body.projectName])
   .then(() => {
@@ -25,12 +19,12 @@ const createUserUUID = async ctx => {
 }
 
 const saveNews = async ctx => {
-  const { rows } = await database.query('select id from project where uuid = $1', [ctx.params.uuid])
+  const { rows } = await database.query('select uuid from project where uuid = $1', [ctx.params.uuid])
   if (rows.length === 0) {
     ctx.throw(404)
   }
 
-  await database.query('insert into posts(project_id, post) values ($1, $2)', [rows[0].id, ctx.request.body.post])
+  await database.query('insert into posts(project_uuid, post) values ($1, $2)', [ctx.params.uuid, ctx.request.body.post])
   .then(() => {
     ctx.body = {
       post: ctx.request.body.post
@@ -45,18 +39,6 @@ const saveNews = async ctx => {
 
 const listNews = ctx => {
   ctx.body = 'List news'
-}
-
-const getUUID = async () => {
-  const maxAttempts = 10
-  for (let i = 0; i < maxAttempts; i++) {
-    const uuid = uuid4()
-    const { rows } = await database.query('select * from project where uuid = $1', [uuid])
-
-    if (rows.length === 0) {
-      return uuid
-    }
-  }
 }
 
 module.exports = {
