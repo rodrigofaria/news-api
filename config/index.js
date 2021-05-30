62244f3bbe3c77803586c5d011db9b2827f8f661
Rodrigo Faria <rodrigo.faria@nexadigital.com.br>
2021-05-30 10:33:51 -0300

    Add ssl configuration in database connection

--- config/index.js
+++ config/index.js
@@ -3,5 +3,6 @@ module.exports = {
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
   host: process.env.DATABASE_HOST,
-  dialect: 'postgres'
+  dialect: 'postgres',
+  ssl: true
 }
